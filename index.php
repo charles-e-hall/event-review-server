763a729cb2e42223b4e929a746ff782a76e5e1ef
Charlie Hall <charlie.hall184@gmail.com>
2020-03-14 18:19:43 -0400

    Update:

--- index.php
+++ index.php
@@ -4,8 +4,20 @@
 
 <section class="main-container">
 	<div class="main-wrapper">
-		<h2>Event Review System</h2>
+		<h2>Apply</h2>
 	</div>
+	<form class="signup-form" method="post" action="inc/signup-inc.php">
+		<input type="text" name="firstname" placeholder="First Name">
+		<input type="text" name="lastname" placeholder="Last Name">
+		<input type="text" name="address" placeholder="Street Address">
+		<input type="text" name="city" placeholder="City">
+		<input type="text" name="state" placeholder="State">
+		<input type="text" name="zipcode" placeholder="Zip Code">
+		<input type="text" name="email" placeholder="E-mail">
+		<input type="password" name="pwd" placeholder="Password">
+		<input type="checkbox" name="agree_to_terms" value="I agree to the terms">
+		<button type="submit" name="submit">Apply</button>
+	</form>
 </section>
 
 <?php
